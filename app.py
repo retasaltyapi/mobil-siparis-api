ed85058acc78e5f23ed311a2bc16a323897482ad
retasaltyapi <100473920+retasaltyapi@users.noreply.github.com>
2025-04-17 01:29:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os  # os modülünü eklemeyi unutmayın
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "API is working!"
+    return "API Çalışıyor!"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))  # Render için port ayarı
+    app.run(host='0.0.0.0', port=port)
